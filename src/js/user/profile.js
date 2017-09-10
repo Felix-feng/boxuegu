1e3f5fa3f5c69303f719e33d961558d37c4fcaca
fable <1873008309@qq.com>
2017-09-10 17:27:33 +0800

    利用模板引擎显示个人资料

--- src/js/user/profile.js
+++ src/js/user/profile.js
@@ -1,1 +1,15 @@
-var a = require('../common/a.js');
+// 导入头部功能
+require('../common/header.js');
+// 导入侧边栏功能
+require('../common/aside.js');
+
+
+$.ajax({
+    url: '/v6/teacher/profile',
+    type: 'get',
+    success: function(data) {
+        if (data.code == 200) {
+            $('.teacher-profile').html(template('teahcer-profile-tpl', data.result));
+        }
+    }
+})

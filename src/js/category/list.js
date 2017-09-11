c13fc9dab5e84f7473482ebe179fd60630f18408
fable <1873008309@qq.com>
2017-09-11 11:48:36 +0800

    修改学科列表

--- src/js/category/list.js
+++ src/js/category/list.js
@@ -2,3 +2,9 @@
 require('../common/header.js');
 // 导入侧边栏功能
 require('../common/aside.js');
+
+
+// 使用jq的ajax插件请求数据
+$.get('/v6/category', function(data) {
+    $('#category_list').append(template('category-list-tpl', data.result));
+})
